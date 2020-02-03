84417a39d055a86d00ebfc523a4240f6d4b70c21
Ryan Pape <>
2020-02-02 23:42:33 -0500

    Makes unit test safe to run in any dir

--- render-test.py
+++ render-test.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 
 import unittest
+import subprocess
+import os
 import render
 from jinja2_ext_custom_autoescaping import CustomAutoescapeExtension
 
 class RenderTest(unittest.TestCase):
 
+	projectPath = subprocess.run(["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+	templatesDir = os.path.join(projectPath, "templates")
+	testOutputDir = os.path.join(projectPath, "test_output")
+
+	def setUp(self):
+		if not os.path.exists(self.testOutputDir):
+			os.makedirs(self.testOutputDir)
+
 	def testLatex(self):
-		yaml = 'test.yaml'
-		template = 'templates/tech-resume.tex'
-		output = 'test_output/tech-resume.tex'
+		yaml = os.path.join(self.projectPath, 'test.yaml')
+		template = os.path.join(self.templatesDir, 'tech-resume.tex')
+		output = os.path.join(self.testOutputDir, 'tech-resume.tex')
 		env = render.getEnvForTemplate(template)
 		self.assertEqual(env.autoescape, render.BUILT_IN_LATEX_AUTOESCAPE)
 		render.renderTemplate(yaml, template, output, env)
 
 	def testHtml(self):
-		yaml = 'test.yaml'
-		template = 'templates/tech-resume.html'
-		output = 'test_output/tech-resume.html'
+		yaml = os.path.join(self.projectPath, 'test.yaml')
+		template = os.path.join(self.templatesDir, 'tech-resume.html')
+		output = os.path.join(self.testOutputDir, 'tech-resume.html')
 		env = render.getEnvForTemplate(template)
 		render.renderTemplate(yaml, template, output, env)
 
